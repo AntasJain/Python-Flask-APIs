a72e66e91fdc0cf3498b9c82ef93c43251da37e2
Antas Jain <jainantas.99@gmail.com>
2024-04-15 13:13:50 -0400

    Completing API
    using flask smorest to create MethodView and blueprint and abort messages, using Marshmellow Schemas for request validations and decorating responses.
    Also restructured the code and implemented swagger ui for API documentation.

--- app.py
+++ app.py
@@ -1,58 +1,17 @@
-from flask import Flask, request
-from db import items,stores
-from flask_smorest import abort
-import uuid
+from flask import Flask
+from flask_smorest import Api
+from resources.item import blp as ItemBlueprint
+from resources.store import blp as StoreBlueprint
 app = Flask(__name__)
 
+app.config["PROPAGATE_EXCEPTIONS"]= True
+app.config["API_TITLE"]="Stores Rest API"
+app.config["API_VERSION"]="v1"
+app.config["OPENAPI_VERSION"]="3.0.3"
+app.config["OPENAPI_URL_PREFIX"]="/"
+app.config["OPENAPI_SWAGGER_UI_PATH"]="/swagger-ui"
+app.config["OPENAPI_SWAGGER_UI_URL"]="https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
 
-
-@app.get("/store") #http://127.0.0.1:5000/store
-def get_stores():
-    return {"stores":list(stores.values())}
-
-@app.post("/store")
-def create_stores():
-    store_data = request.get_json()
-    if "name" not in store_data:
-        abort(400,message="name must be included in the payload")
-    for store in stores.values():
-        if store_data["name"]==store["name"]:
-            abort(400,message="Store already Exists")
-    store_id = uuid.uuid4().hex
-    store = {**store_data,'id':store_id}
-    stores[store_id]=store
-    return store,201
-
-@app.post("/item")
-def create_item(name):
-    item_data = request.get_json()
-    if('price' not in item_data or 'store_id' not in item_data or 'name' not in item_data):
-        abort(400,message="bad request. Ensure 'price', 'store_id' and 'name' are included in the JSON playload")
-    if(item_data["store_id"] not in stores):
-        abort(404, message="Store not found")
-    for item in items.values():
-        if item_data["name"]==item["name"] and item_data["store_id"]==item["store_id"]:
-           abort(400,message="Item already exist") 
-    item_id = uuid.uuid4().hex
-    item = {**item_data,'id':item_id}
-    items[item_id]=item
-    return item,201
-    
-@app.get("/items")
-def get_all_items():
-       return {"items":list(items.values())} 
-
-@app.get("/store/<string:store_id>")
-def get_store(store_id):
-    try:
-        return stores[store_id],201
-    except KeyError:
-        abort(404,message="Store not found")
-    
-
-@app.get("/item/<string:item_id>")
-def get_store_items(item_id):
-    try:
-        return items[item_id],201
-    except KeyError:
-        abort(404,message="item not found")
+api = Api(app)
+api.register_blueprint(ItemBlueprint)
+api.register_blueprint(StoreBlueprint)
